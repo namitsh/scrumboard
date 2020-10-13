8a52f8232362951b09813f282e49d43ff46e7157
Namit Sharda <namit.sharda63@gmail.com>
2020-10-14 02:05:08 +0530

    Adds Task and User Endpoints

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
-from .models import Sprint
+from .models import Sprint,Task
+
+
+User = get_user_model()
+
 
 class SprintSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +14,24 @@ class SprintSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'description', 'end', )
 
     
+class TaskSerializer(serializers.ModelSerializer):
+
+    status_display = serializers.SerializerMethodField()
+    assigned = serializers.SlugRelatedField(slug_field= User.USERNAME_FIELD, required = False)
+    class Meta:
+        model = Task
+        fields = ('id', 'name', 'description', 'sprint', 'status' ,
+        'order' , 'status_display','assigned' , 'started' , 'due' , 'completed' ,)
+    
+    def get_status_display(self,obj):
+        return obj.get_status_display()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active',)
+
+        
--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
 from rest_framework import viewsets, authentication, permissions
 
-from .models import Sprint
-from .serializers import SprintSerializer
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
 # Create your views here.
+
+
+User = get_user_model()
 
 class DefaultMixin(object):
     """Default settings for view authentication, permission, filtering, and pagination"""
@@ -27,4 +30,21 @@ class SprintViewSet(DefaultMixin,viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+
+
+class TaskViewSet(DefaultMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
+
+
+
 
