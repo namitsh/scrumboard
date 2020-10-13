3352c238900cf37906e53721357ccf8955e973ca
Namit Sharda <namit.sharda63@gmail.com>
2020-10-14 01:31:56 +0530

    Adds Sprint Endpoints and creates DefaultMixin class

--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets, authentication, permissions
+
+from .models import Sprint
+from .serializers import SprintSerializer
+
 # Create your views here.
+
+class DefaultMixin(object):
+    """Default settings for view authentication, permission, filtering, and pagination"""
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication
+    )
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param= 'page_size'
+    max_paginate_by = 100
+
+
+
+class SprintViewSet(DefaultMixin,viewsets.ModelViewSet):
+    """API endpoints for listing and creating sprints"""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+
